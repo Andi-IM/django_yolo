5fa2629a4c2345427f8ea65fda00259d2a835df6
andi-IM <andi.irhamm@gmail.com>
2023-02-13 19:57:15 +0700

    update urls

--- objectdetection/urls.py
+++ objectdetection/urls.py
@@ -11,7 +11,7 @@ router.register('users', views.UserViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api_auth', views.object_detection_api, name='detect'),
+    path('api_request', views.object_detection_api, name='detect'),
     path('', views.detect_request),
     re_path(r'^detection/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT,
